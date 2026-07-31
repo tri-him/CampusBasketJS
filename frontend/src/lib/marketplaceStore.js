d09e83a778ade4cbc35ef47e43dbbbfbf6e0bbde
lamar <lamartriddles@gmail.com>
2026-07-31 14:09:41 +0400

    disappearing

--- frontend/src/lib/marketplaceStore.js
+++ frontend/src/lib/marketplaceStore.js
@@ -354,21 +354,8 @@ export const getMarketplaceProducts = () => getProductCache();
 
 export const syncMarketplaceProducts = async (params = {}) => {
   const response = await productApi.list(params);
-
-  console.log("PRODUCT API RESPONSE:", response);
-
-  const rawProducts =
-    response.data ||
-    response.products ||
-    response.items ||
-    response;
-
-  const products = Array.isArray(rawProducts)
-    ? rawProducts.map(normalizeProductRecord)
-    : [];
-
+  const products = (response.data || []).map(normalizeProductRecord);
   saveProducts(products);
-
   return products;
 };
 
