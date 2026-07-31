6f62cc82551613006e3af489aa238f7e2e52b8ea
lamar <lamartriddles@gmail.com>
2026-07-31 15:27:06 +0400

    publishing issues

--- backend/src/app.js
+++ backend/src/app.js
@@ -28,7 +28,12 @@ app.use(
 );
 
 // Standard body parsing & logging middlewares
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({
+  extended: true,
+  limit: "50mb"
+}));
+
 app.use(morgan("dev"));
 
 // API Router
